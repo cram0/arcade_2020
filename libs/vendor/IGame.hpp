5a431a7cecd2df3b5abab09539f728e60f09fc4d
cram <julien.augugliaro@epitech.eu>
2021-03-19 14:48:16 +0100

    Added: Vendor folder for Interfaces

---
+++ libs/vendor/IGame.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2021
+** B-OOP-400-NCE-4-1-arcade-marius.contoli-legay
+** File description:
+** IGame
+*/
+
+#ifndef IGAME_HPP_
+#define IGAME_HPP_
+
+class IGame {
+    public:
+        virtual ~IGame() = default;
+
+    protected:
+    private:
+};
+
+#endif /* !IGAME_HPP_ */
---
+++ libs/vendor/IGraphic.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2021
+** B-OOP-400-NCE-4-1-arcade-marius.contoli-legay
+** File description:
+** IGraphic
+*/
+
+#ifndef IGRAPHIC_HPP_
+#define IGRAPHIC_HPP_
+
+class IGraphic {
+    public:
+        virtual ~IGraphic() = default;
+
+    protected:
+    private:
+};
+
+#endif /* !IGRAPHIC_HPP_ */
